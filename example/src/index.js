08d995b4930eafc21499569762abc62ef6e30d47
Jonathan Julian <jonathan@adhocteam.us>
2017-12-01 14:48:58 -0800

    Expand the list of redact keys (#5)

--- example/src/index.js
+++ example/src/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const log = require('./log');
 const logger = log.logger;
@@ -6,7 +6,11 @@ const logger2 = log.contextLogger({ id: 222222 });
 const logger3 = log.contextLogger({ id: 333333, server: 'test-a' });
 
 logger.warn('example warning', { status: 502 });
-logger.info('example info', { pizza: true, toppings: ['mushrooms, olives'] });
+logger.info('example info', {
+    pizza: true,
+    toppings: ['mushrooms, olives'],
+    pepper: 'hot'
+});
 logger2.info('example info', { pizza: false });
 logger3.info('example info', { pizza: true, toppings: [] });
 logger2.info('example info', { pizza: false, status: 404 });
