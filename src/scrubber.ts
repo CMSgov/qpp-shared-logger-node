d25550b4faf47da9d511ad6ecdc0578769b65b0c
Jesse Kawell <jakawell@users.noreply.github.com>
2022-01-11 13:00:52 -0500

    Add support for redactions via regular expression (#28)

--- src/scrubber.ts
+++ src/scrubber.ts
@@ -22,12 +22,13 @@ const defaultRedactKeys = [
 ];
 
 export class Scrubber {
-    blacklist = [];
+    private readonly blacklist: string[] = [];
+    private readonly regexesBlacklist: Array<string | RegExp> = [];
     // A winston Formatter that removes all meta
     // See https://github.com/winstonjs/winston/blob/HEAD/UPGRADE-3.0.md#rewriters for upgrade info
     format = winston.format((info) => this.scrub(info));
 
-    constructor(redactKeys: string[]) {
+    constructor(redactKeys: string[], regexesBlacklist?: Array<string | RegExp>) {
         // merge with defaults
         this.blacklist = [
             ...new Set([
@@ -35,6 +36,7 @@ export class Scrubber {
                 ...defaultRedactKeys,
             ]),
         ];
+        this.regexesBlacklist = regexesBlacklist ?? [];
     }
 
     scrub(logEntry) {
@@ -42,6 +44,13 @@ export class Scrubber {
         const redacted = _.cloneDeepWith(logEntry, (value, key) => {
             if (_.isString(key) && this.blacklist.includes(key.toLowerCase())) {
                 return '[REDACTED]';
+            }
+            if (_.isString(value) && this.regexesBlacklist) {
+                this.regexesBlacklist.forEach((redaction) => {
+                    const regex = new RegExp(redaction);
+                    value = value.replace(new RegExp(regex.source, regex.flags + 'g'), '[REDACTED]');
+                })
+                return value;
             }
         });
 
